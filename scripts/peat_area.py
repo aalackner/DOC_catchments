641629e646fd4b610119c7ba6c44afe266c0fca0
Anna Lackner <anna.lackner@slu.se>
2025-04-30 14:41:46 +0200

    complete peat cover etraction from command line

--- scripts/peat_area.py
+++ scripts/peat_area.py
@@ -2,42 +2,117 @@
 #%% Only needed if used from terminal, otherwise the first block needs to be commented out
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("r", type=str, help="the raster")
-parser.add_argument("c", type=str, help="the catchments")
-parser.add_argument("o", type=str, help="the output")
+parser.add_argument("-r", type=str, help="the location for the mosaic")
+parser.add_argument("-c", type=str, help="the catchments")
+parser.add_argument("-o", type=str, help="the output")
+parser.add_argument("-id", type=str, help="the id variable", default="mvmid")
+parser.add_argument("-gdb", type=str, help="the gdb to use for intermediate steps", default="test.gdb")
 args = parser.parse_args()
-# %%
-import rasterstats as rs
-import geopandas as gpd
-import pandas as pd
+
+#%% load the environment variabl
+import arcpy
+from arcpy import env
+from arcpy.sa import *
+
+import sys
+
+#%%
+
+# class Args:
+#     pass
+
+# args = Args()
+
+# args.id = "mvmid"  # Default value for id variable
 
 
-# Load catchments (polygons)
-gdf_catch = gpd.read_file("zip://DOC_catchments//input/shapefile/catchments.zip")
 
-mvm_ids = [ 1815, 1329 ]
-
-gdf_catch = gdf_catch.loc[gdf_catch['mvm_id'].isin(mvm_ids)]
-
-# gdf_catch = gpd.read_file(args.c)
+# # # Example manual assignments
+# args.c = r"C:\Users\anlr0006\repos-win\DOC_catchments\data\aro_trendsjoar_106_250408.zip"
+# args.o = r"C:\Users\anlr0006\repos-win\DOC_catchments\results\slu_sgu\ditch_test.csv"
+# args.r = r"C:\Users\anlr0006\repos-win\DOC_catchments\input\Dikeskarta\Mosaic_ditches.gdb"
+# args.gdb = r"C:\Users\anlr0006\repos-win\DOC_catchments\results\arcpy_workspace\test_ditches.gdb"  # Default value for gdb
 
 
-## Need to mount network drive to gis. 
 
-# Path to raster
-raster_path = r"\\gis.slu.se\gisdata\slu\Torvkarta_1_0\Klassad_torvkarta\ClassifiedPeatMap.tif"
 
-# Calculate zonal statistics (area for each class in the raster)
-stats = rs.zonal_stats(gdf_catch, raster_path, stats="sum", band = 1, categorical  = True,  geojson_out = True)
+#%% set the workspace and populate the gdb
+import os.path
 
-# Display the results
-print("done")
+# arcpy.management.CreateFileGDB(os.path.dirname(args.gdb), os.path.basename(args.gdb), "CURRENT")
+arcpy.env.workspace = os.path.join(args.gdb)
 
-out = pd.json_normalize(pd.DataFrame(stats)['properties'])
-out['sum'] = pd.to_numeric(out['sum'], errors = 'coerce')
-out['Shape_Area'] = pd.to_numeric(out['Shape_Area'], errors = 'coerce')
-out['coverage'] = out['Shape_Area']/(4 * out['sum']) * 100
+# SET PYTHON WORKING DIR
+#os.chdir(os.path.join(r"C:\Users\anlr0006\repos-win\DOC_catchments\results\arcpy_workspace", args.gdb)) 
 
-out
-# out.to_csv('peat_area_by_class_all.csv')
-# %%
+arcpy.env.overwriteOutput = True
+
+from zipfile import ZipFile
+import os
+
+if os.path.exists(arcpy.env.workspace) == False:
+    arcpy.management.CreateFileGDB(os.path.dirname(args.gdb), os.path.basename(args.gdb), "CURRENT")
+
+    with ZipFile(args.c, 'r') as zf:
+        for file in zf.namelist():
+            extracted_path = os.path.join(arcpy.env.workspace, file)
+            if os.path.exists(extracted_path):
+                os.remove(extracted_path)  # Remove the existing file
+            zf.extract(file, arcpy.env.workspace)
+
+    # Go into the folder and rename any ö to o
+    # Rename files with 'ö' to 'o'
+    for filename in os.listdir(arcpy.env.workspace):
+        if 'ö' in filename:
+            os.rename(
+                os.path.join(arcpy.env.workspace, filename),
+                os.path.join(arcpy.env.workspace, filename.replace('ö', 'o'))
+            )
+
+# Get the shapefile name
+shapefile = os.path.join(arcpy.env.workspace,os.path.splitext(os.path.basename(args.c))[0].replace('ö', 'o') + ".shp")
+
+
+#%% Check id variable and availability of rasters
+# ID
+# Check if the specified field exists in the shapefile
+fields = [field.name for field in arcpy.ListFields(shapefile)]
+
+if args.id not in fields:
+    print(f"Field '{args.id}' not found in the shapefile '{args.c}'.")
+    print("Available fields in the shapefile:")
+    for field in fields:
+        print(f"- {field}")
+else:
+    print(f"Field '{args.id}' exists in the shapefile '{args.c}'.")
+
+# check 
+
+#%% Peat area
+
+out_table =os.path.join(arcpy.env.workspace,r"output_table_peat")
+
+print(f"Shapefile: {shapefile}")
+print(f"Raster: {args.r}")
+print(f"Output Table: {out_table}")
+
+arcpy.ia.ZonalStatisticsAsTable(
+    in_zone_data=shapefile,
+    zone_field=args.id,
+    in_value_raster=args.r,
+    out_table=out_table,
+    ignore_nodata="DATA",
+    statistics_type="ALL",
+    process_as_multidimensional="CURRENT_SLICE",
+    percentile_values=[25,75],
+    percentile_interpolation_type="AUTO_DETECT",
+    circular_calculation="ARITHMETIC",
+    circular_wrap_value=360,
+    out_join_layer=None
+)
+
+# Convert the output table to a CSV file
+csv_output = args.o  # Ensure args.o ends with ".csv"
+
+arcpy.conversion.ExportTable(in_table = out_table, 
+                             out_table = csv_output)
